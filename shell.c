f65b053b65dc8355f22e172653768a6ff9a84c5c
Ayoub <ay.belkacem@gmail.com>
2023-09-20 00:16:36 +0100

    Enhanced interactive shell functionality and optimized code.

--- shell.c
+++ shell.c
@@ -1,9 +1,10 @@
 #include "header.h"
+
 /**
- * execute_command - function that execute command
- * @args: the array contain the command
- * Return: 0 or 127 if it fail
-*/
+ * execute_command - Executes a command specified by the args array.
+ * @args: The array containing the command and its arguments.
+ * Return: The exit status of the executed command.
+ */
 int execute_command(char **args)
 {
 	pid_t pid;
@@ -25,15 +26,15 @@ int execute_command(char **args)
 			}
 			else
 			{
-				write(STDERR_FILENO, "./hsh: 1: ", 11);
-				write(STDERR_FILENO, args[0], strlen(args[0]));
-				write(STDERR_FILENO, ": not found\n", 13);
+				fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
 				exit(127);
 			}
 		}
 	}
 	else if (pid < 0)
+	{
 		perror("fork");
+	}
 	else
 	{
 		wait(&status);
@@ -45,59 +46,53 @@ int execute_command(char **args)
 	}
 	return (0);
 }
+
 /**
- * shell_interactive - function loop
- *
- * Return: status
-*/
+ * shell_interactive - Handles the interactive mode of the shell.
+ * Return: The exit status of the shell.
+ */
 int shell_interactive(void)
 {
-	char **args;
-	char *buffer = NULL;
-	size_t buf_size = 0;
-	int n_read, status;
-	int i, contain_all_space;
+	char **args, *buf = NULL;
+	size_t size = 0;
+	int n, status, i, is_empty;
 
-	while (1)
+	while (printf("#cisfun$ "), n = getline(&buf, &size, stdin), n != -1)
 	{
-		write(STDOUT_FILENO, "#cisfun$ ", 10);
-		n_read = getline(&buffer, &buf_size, stdin);
-		if (n_read == -1)
-		{
-			if (feof(stdin))
-			{
-				write(1, "\n", 1), free(buffer);
-				exit(status);
-			}
-			else
-				perror("getline failed"), exit(1);
-		}
-		buffer[n_read - 1] = '\0';
-		contain_all_space = 1;
-		for (i = 0; i < n_read - 1; i++)
-		{
-			if (buffer[i] != ' ')
-				contain_all_space = 0;
-		}
-		if (contain_all_space) /* Handle the case where the input is only spaces*/
-			continue; /* Skip processing and prompt for the next input*/
-		args = split_array(buffer, strlen(buffer));
+		if (n > 1 && buf[n - 1] == '\n')
+			buf[--n] = 0;
+		is_empty = 1;
+
+		for (i = 0; i < n; i++)
+			if (buf[i] != ' ')
+				is_empty = 0;
+		if (is_empty)
+			continue;
+		args = split_array(buf, n);
 		if (strcmp(args[0], "env") == 0)
 			status = print_env();
 		else if (strcmp(args[0], "exit") == 0)
-			free(buffer), handle_exit(args);
+			free(buf), handle_exit(args);
 		else
+		{
 			status = execute_command(args);
+			if (status == 127)
+				fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
+		}
 		free_args(args);
 	}
-	return (status);
+	if (n == -1 && feof(stdin))
+		printf("\n"), free(buf), exit(status);
+	perror("getline failed");
+	exit(1);
 }
 
+
+
 /**
- * shell_non_interaction - function that handle
- * non interactive mode
- * Return: status
-*/
+ * shell_non_interaction - Handles the non-interactive mode of the shell.
+ * Return: The exit status of the shell.
+ */
 int shell_non_interaction(void)
 {
 	char *line = NULL;
@@ -107,14 +102,21 @@ int shell_non_interaction(void)
 
 	while (getline(&line, &size, stdin) > 0)
 	{
+		/* Split the input into arguments */
 		args = split_array(line, strlen(line));
+
+		/* Check for built-in commands or execute external commands */
 		if (strcmp(args[0], "exit") == 0)
 			handle_exit(args);
 		if (strcmp(args[0], "env") == 0)
 			status = print_env();
 		else
+		{
+			/* Execute the command and handle errors */
 			status = execute_command(args);
+			if (status == 127)
+				fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
+		}
 	}
 	return (status);
 }
-
